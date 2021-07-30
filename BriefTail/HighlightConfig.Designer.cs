35acbb537588ca0bb7875943fffcbb41c281ded3
tangmengjin <tangmengjin@laiye.com>
2021-07-30 20:08:58 +0800

    修改高亮窗口的宽度，不允许最大化最小化

--- BriefTail/HighlightConfig.Designer.cs
+++ BriefTail/HighlightConfig.Designer.cs
@@ -56,9 +56,11 @@ namespace BriefTail
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(422, 450);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.addButton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "HighlightConfig";
             this.Text = "HighlightConfig";
             this.ResumeLayout(false);
